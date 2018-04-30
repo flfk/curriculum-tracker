13424960c066cbd561e2537a592594b5b2fb26de
flfk <flfk@users.noreply.github.com>
2018-04-30 16:12:53 -0300

    retrieving subjects from static JSON data

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 
+import Subject from './Subject';
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      subjectName: 'Harvard cs50',
+      subjectIDs: ['0', '1'],
     };
   }
 
   render() {
+    const subjects = this.state.subjectIDs
+      .map(elem => <Subject key= {elem} subjectID={elem} />);
+
     return (
       <div className="App">
         <p>hello world</p>
-        { this.state.subjectName }
+        { this.state.subjectIDs[0] }
+        <div>{ subjects }</div>
       </div>
     );
   }
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -1,7 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import SubjectModel from '../model/SubjectModel.json';
+
+const subjectData = SubjectModel;
+
+
 const Subject = (props) => {
-  (
-    'test'
+  const { subjectID } = props;
+  const subject = subjectData[subjectID];
+
+  return (
+    <div>
+      { subject.name }
+    </div>
   );
+};
+
+Subject.propTypes = {
+  subjectID: PropTypes.string,
+};
+Subject.defaultProps = {
+  subjectID: '',
 };
 
 export default Subject;
