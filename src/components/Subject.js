f60444d1745d024c3bc3ab6d333f69426a711a0a
flfk <flfk@users.noreply.github.com>
2018-05-19 17:54:52 -0300

    Subject tag images displaying

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -4,16 +4,39 @@ import PropTypes from 'prop-types';
 import SubjectModel from '../model/SubjectModel.json';
 import '../styles/subject.css';
 
+import iconOnlineCourse from '../icons/IconScreen.png';
+import iconDuration from '../icons/IconHourGlass.png';
+import iconAuthor from '../icons/IconFeather.png';
+
 const subjectData = SubjectModel;
 
 const SubjectTag = (props) => {
-  const { subjectTag } = props;
+  const { tagName } = props;
+  const { tagValue } = props;
 
-  // add logic to retrieve icons from ../public and add to return
+  // Test if tag is empty
+  if (tagValue === '') {
+    return (<div />);
+  }
+
+  // Return correct icon
+  let tagIcon = '';
+  if (tagName === 'type') {
+    if (tagValue === 'Online Course') {
+      tagIcon = iconOnlineCourse;
+    }
+  }
+  if (tagName === 'duration') {
+    tagIcon = iconDuration;
+  }
+  if (tagName === 'author') {
+    tagIcon = iconAuthor;
+  }
 
   return (
     <div className="subjectTag">
-      {subjectTag}
+      <img src={tagIcon} alt={`${tagValue} icon`} />
+      {tagValue}
     </div>
   );
 };
@@ -23,15 +46,22 @@ const Subject = (props) => {
   const subject = subjectData[subjectID];
 
   const { type, duration, author } = subject;
-  const subjectTags = [type, duration, author]
-    .filter(elem => elem !== '')
-    .map(elem => <SubjectTag key={elem} subjectTag={elem} />);
+
+  const typeTag = <SubjectTag key={type} tagName='type' tagValue={type} />;
+  const durationTag = <SubjectTag key={duration} tagName='duration' tagValue={duration} />;
+  const authorTag = <SubjectTag key={author} tagName='author' tagValue={author} />;
+
+  const subjectTags = typeTag;
 
   return (
     <div className="subject">
       <button className="tickBtn" />
       <div className="subjectName">{ subject.name }</div>
-      <div className="subjectTagsDiv">{ subjectTags }</div>
+      <div className="subjectTagsDiv">
+        <div className="subjectTag">{ typeTag }</div>
+        <div className="subjectTag">{ durationTag }</div>
+        <div className="subjectTag">{ authorTag }</div>
+      </div>
     </div>
   );
 };
